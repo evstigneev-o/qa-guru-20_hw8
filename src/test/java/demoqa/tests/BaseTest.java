f29c1abd06420dac20388a6c4284c555bb17e673
Oleg Evstigneev <o.evstigneev@citydrive.ru>
2023-06-29 21:27:41 +0300

    add allure report

--- src/test/java/demoqa/tests/BaseTest.java
+++ src/test/java/demoqa/tests/BaseTest.java
@@ -2,8 +2,15 @@ package demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.github.javafaker.Faker;
+import demoqa.helpers.Attach;
 import demoqa.pages.StudentRegistrationPage;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class BaseTest {
 
@@ -18,5 +25,20 @@ public class BaseTest {
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
         //Configuration.headless = true;
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+    }
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
+        closeWebDriver();
     }
 }
